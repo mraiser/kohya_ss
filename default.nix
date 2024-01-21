505a08f50e78617a54f2b617af52336a5dc47db9
Marc Raiser <marc@raiser.us>
2024-01-21 18:20:08 -0500

    default.nix enables successful build on nix

---
+++ default.nix
@@ -0,0 +1,25 @@
+with (import <nixpkgs> {});
+let
+  LLP = with pkgs; [
+    gcc-unwrapped
+    cudatoolkit_11.out
+    cudatoolkit_11.lib
+    linuxPackages.nvidia_x11
+    gperftools
+    libGL
+    glib
+    zlib
+    python310
+    python310Packages.tkinter
+  ];
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath LLP;
+in  
+stdenv.mkDerivation {
+  name = "python-ai-env";
+  buildInputs = LLP;
+  src = null;
+  shellHook = ''
+    SOURCE_DATE_EPOCH=$(date +%s)
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
+  '';
+}
